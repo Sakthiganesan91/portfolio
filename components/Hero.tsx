f022d94ca6613e838e5f59022ca6ff9e94dc9d9c
Sakthiganesan <sakthiganesan158@gmail.com>
2024-07-02 18:46:40 +0530

    completed hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import ShimmerButton from "./ui/shimmer-button";
 import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/text-generate-effect";
 
 function Hero() {
+  const words = "Sakthiganesan, Full Stack Developer";
   return (
-    <div className="pb-20 pt-36">
+    <div className="pb-20 pt-10">
       <div>
         <Spotlight
           className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
@@ -24,8 +27,27 @@ function Hero() {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <h2 className="uppercase tracking-widest text-xs text-center text-blue-200 max-w-80">
-            Dynmaic Web Magic with Next.js
+            Blending Technology with Perfection
           </h2>
+          <TextGenerateEffect
+            words={words}
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+          />
+          <p className="text-center text-white md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            Guy Passionate about Computer Technology
+          </p>
+          <div className="flex">
+            <div className="mx-2">
+              <a href="https://github.com/Sakthiganesan91">
+                <ShimmerButton buttonName="Github" />
+              </a>
+            </div>
+            <div>
+              <a href="https://www.linkedin.com/in/sakthiganesang/">
+                <ShimmerButton buttonName="Linkedin" />
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
