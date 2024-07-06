20755d329577cad30b98c712764e04c4e9476a14
Sakthiganesan <sakthiganesan158@gmail.com>
2024-07-06 19:40:53 +0530

    modified projects

--- data/index.ts
+++ data/index.ts
@@ -16,37 +16,37 @@ export const gridItems = [
 ];
 
 export const projects = [
-  {
-    id: 1,
-    title: "3D Solar System Planets to Explore",
-    des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
-    img: "/p1.svg",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-    link: "https://github.com/adrianhajdin?tab=repositories",
-  },
-  {
-    id: 2,
-    title: "Yoom - Video Conferencing App",
-    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
-    img: "/p2.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
-    link: "https://github.com/adrianhajdin/zoom-clone",
-  },
+  // {
+  //   id: 1,
+  //   title: "3D Solar System Planets to Explore",
+  //   des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
+  //   img: "/p1.svg",
+  //   iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+  //   link: "https://github.com/adrianhajdin?tab=repositories",
+  // },
+  // {
+  //   id: 2,
+  //   title: "Yoom - Video Conferencing App",
+  //   des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+  //   img: "/p2.svg",
+  //   iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+  //   link: "https://github.com/adrianhajdin/zoom-clone",
+  // },
   {
     id: 3,
-    title: "AI Image SaaS - Canva Application",
+    title: "AI Image - PhotoFy Application",
     des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-    img: "/p3.svg",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-    link: "https://github.com/adrianhajdin/ai_saas_app",
+    img: "/photofy.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "https://github.com/Sakthiganesan91/PhotoFy",
   },
   {
     id: 4,
-    title: "Animated Apple Iphone 3D Website",
-    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-    img: "/p4.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-    link: "https://github.com/adrianhajdin/iphone",
+    title: "Hey Community",
+    des: "A website for self-learners, where users can join chat rooms to discuss and also post blogs, and library notes.",
+    img: "/oneplace.png",
+    iconLists: ["/re.svg", "/nodejs.svg", "mongodb.svg"],
+    link: "https://github.com/Sakthiganesan91/oneplacebackend",
   },
 ];
 
