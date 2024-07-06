035ac061aa1da712e49a2d006f919e96d8d3e6fa
Sakthiganesan <sakthiganesan158@gmail.com>
2024-07-06 19:47:57 +0530

    modified projects

--- components/RecentProject.tsx
+++ components/RecentProject.tsx
@@ -18,8 +18,8 @@ const RecentProjects = () => {
             key={item.id}
           >
             <PinContainer
-              title="/ui.aceternity.com"
-              href="https://twitter.com/mannupaaji"
+              title="Sakthiganesan G"
+              href="https://github.com/Sakthiganesan91"
             >
               <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
                 <div
